6274ab7accc2deb6cb713d5cd5ca929215cc0bd5
lindsay gallagher <lgallagher883@gmail.com>
2026-08-02 16:18:54 +0100

    Redirect homepage to registration

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>LEO AI</h1>
-
-      <p>System loaded</p>
-
-      <div style={{ marginTop: 20 }}>
-        <Link href="/dashboard">Go to Dashboard</Link>
-        <br />
-        <Link href="/ask">Ask Leo</Link>
-      </div>
-    </main>
-  );
+  redirect("/register");
 }
